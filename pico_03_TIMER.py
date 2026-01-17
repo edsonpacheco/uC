cdbdd339bd6df42c7b6de976c3a2516bbdb8cbd8
edsonpacheco <128686857+edsonpacheco@users.noreply.github.com>
2026-01-17 18:55:11 -0300

    Implement timer to toggle LED every 500ms

---
+++ pico_03_TIMER.py
@@ -0,0 +1,22 @@
+from machine import Pin, Timer
+import machine
+
+led = Pin(15, Pin.OUT)
+# O ID -1 cria um timer virtual no MicroPython
+timer = Timer(-1)
+
+# Função de Callback (ISR)
+def timer_callback(t):
+    led.toggle()
+    estado = "ON" if led.value() else "OFF"
+    # Removida a barra invertida (\) que causava erro no Python
+    print("LED: %s" % estado)
+
+# Inicializa timer de 500ms
+timer.init(period=500, 
+           mode=Timer.PERIODIC, 
+           callback=timer_callback)
+
+while True:
+    # idle() é mais estável para o console do simulador
+    machine.idle()
