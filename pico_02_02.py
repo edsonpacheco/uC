cd9530b2fdc3b9a49661935913513fa008a95b2c
edsonpacheco <128686857+edsonpacheco@users.noreply.github.com>
2026-01-24 15:53:14 -0300

    Change LED pin assignments and logic for temperature

--- pico_02_02.py
+++ pico_02_02.py
@@ -1,9 +1,8 @@
 from machine import Pin, ADC
 import utime
-
 # Sensor no GP26 (ADC 0)
 sensor = ADC(26)
-led_v = Pin(14, Pin.OUT)
+led_v = Pin(12, Pin.OUT)
 led_r = Pin(13, Pin.OUT)
 
 while True:
@@ -12,8 +11,8 @@ while True:
     temp = (raw / 65535.0) * 100.0
     
     quente = temp > 40.0
-    led_v.value(quente)
-    led_r.value(not quente)
+    led_v.value(not quente)
+    led_r.value( quente)
     
     print("Temp: {:.1f} C".format(temp))
     utime.sleep_ms(500)
